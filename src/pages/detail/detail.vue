7e8734166d514400e4520df2075ab857bafc7fa1
halo <haloqq8959@163.com>
2019-03-06 10:25:50 +0800

    0306

---
+++ src/pages/detail/detail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="DetailContainer">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'detail'
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
